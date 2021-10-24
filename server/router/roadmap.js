c307205a3f21de1f28f3211b7ba418270c310bb6
AbhishekTiwari07 <abhishekt2024@gmail.com>
2021-10-24 17:08:24 +0530

    Added new routes

--- server/router/roadmap.js
+++ server/router/roadmap.js
@@ -52,4 +52,70 @@ router.post('/addNewStep/:id', auth, async (req,res)=>{
     }
 });
 
+router.get('/:id', async (req,res)=>{
+    try{
+        const rm = await Roadmap.findOne({
+            id: req.params.id
+        }).populate('steps')
+
+        res.status(200).send(rm);
+    }
+    catch(e){
+        res.status(400).json({
+            message: e.message
+        });
+    }
+})
+
+router.put('/:id', async (req,res)=>{
+    try{
+        var rm = await Roadmap.findOneAndUpdate({
+            id: req.params.id
+        },{
+            ...req.body
+        });
+
+        res.status(200).send(rm);
+    }
+    catch(e){
+        res.status(400).json({
+            message: e.message
+        });
+    }
+})
+
+router.get('/step/:id', async (req,res)=>{
+    try{
+        const step = await Step.findOne({
+            id: req.params.id
+        })
+
+        res.status(200).send(step);
+    }
+    catch(e){
+        res.status(400).json({
+            message: e.message
+        });
+    }
+})
+
+router.put('/step/:id', async (req,res)=>{
+    try{
+        var step = await Step.findOneAndUpdate({
+            id: req.params.id
+        }, 
+        {
+            ...req.body
+        });
+
+
+        res.status(200).send(step);
+    }
+    catch(e){
+        res.status(400).json({
+            message: e.message
+        });
+    }
+});
+
 module.exports = router
