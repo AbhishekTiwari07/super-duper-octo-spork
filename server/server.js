4d0a52350f77cb403455639ba60bffbb21750cf1
AbhishekTiwari07 <abhishekt2024@gmail.com>
2021-10-25 22:36:48 +0530

    Added cors

--- server/server.js
+++ server/server.js
@@ -9,9 +9,9 @@ const roadmap = require('./router/roadmap');
 
 const app = express();
 
-app.use(express.urlencoded({extended:false}))
-app.use(express.json())
-app.use(cors())
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
